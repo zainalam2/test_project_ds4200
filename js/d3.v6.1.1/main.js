6fb15680a44558f8644dc7a47eda923ede445960
jack-krolik <belljack17@gmail.com>
2023-03-20 16:17:03 -0400

    fixed scatter

--- js/d3.v6.1.1/main.js
+++ js/d3.v6.1.1/main.js
@@ -14,20 +14,24 @@ let SCATTER_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
 
 // Reading from file and appending points
 d3.csv("data/Spotify_Songs_Subset.csv").then((data) => {
-    
+    let keys = {'A' : 'blue', 'A#/Bb' : 'mediumturquiose', 
+    'B' : 'green', 'C' : 'orange', 'C#/Db' : 'coral', 
+    'D' : 'red', 'D#/Eb' : 'magenta', 'E' : 'violet', 
+    'F' : 'brown', 'F#/Gb' : 'tomato', 'G' : 'pink', 'G#/Ab' : 'mediumslateblue'}
+
     // Getting max X and Y coords
     const MAX_X = d3.max(data, (d) => 
-                                {return parseFloat(d.loudness)});
+                                {return Math.abs(parseFloat(d.loudness))});
                                 
     const MAX_Y = d3.max(data, (d) => 
-                                {return parseFloat(d.danceability)});
+                                {return Math.abs(parseFloat(d.acousticness))});
 
     // Getting max X and Y coords
     const MIN_X = d3.min(data, (d) => 
-                                {return parseFloat(d.loudness)});
+                                {return  Math.abs(parseFloat(d.loudness))});
                                 
     const MIN_Y = d3.min(data, (d) => 
-                                {return parseFloat(d.danceability)});
+                                {return Math.abs(parseFloat(d.acousticness))});
 
     console.log(MAX_X)
     console.log(MIN_X)
@@ -35,47 +39,43 @@ d3.csv("data/Spotify_Songs_Subset.csv").then((data) => {
     console.log(MIN_Y)
     // X coord scale function
     const X_SCALE = d3.scaleLinear()
-                            .domain([0, (MAX_X)])
-                            .range([SCATTER_WIDTH/2, SCATTER_WIDTH]);
+                            .domain([0, MAX_X])
+                            .range([0, SCATTER_WIDTH]);
 
-    const X_SCALE2 = d3.scaleLinear()
-                        .domain([(-MAX_X), (MAX_X)])
-                        .range([(MIN_X), SCATTER_WIDTH]);
     
     // Y coord scale function
     const Y_SCALE = d3.scaleLinear()
-                        .domain([0, (MAX_Y)])
-                        .range([SCATTER_HEIGHT/2, 0]);
+                        .domain([0, MAX_Y])
+                        .range([SCATTER_HEIGHT, 0]);
 
-    const Y_SCALE2 = d3.scaleLinear()
-                        .domain([(-MAX_Y), (MAX_Y)])
-                        .range([(SCATTER_HEIGHT), 0]);
+  
 
     // plot the scatter points
     let Points = SCATTER_FRAME.selectAll("circle")
             .data(data)
             .enter()
             .append("circle")
-                .attr("cx", (d) => {return (X_SCALE(d.loudness) + MARGINS.left + 1)})
-                .attr("cy", (d) => {return ((Y_SCALE(d.danceability)) + MARGINS.top)})
+                .attr("cx", (d) => {return (X_SCALE(Math.abs(parseFloat(d.loudness))) + 2 * MARGINS.left)})
+                .attr("cy", (d) => {return ((Y_SCALE(Math.abs(parseFloat(d.acousticness))) + MARGINS.top))})
                 .attr("r", 1)
                 .attr("id", (d) => {return (d.track_name)});
+                // .style("fill", (d) => {return (console.log((Math.abs(parseFloat(d.loudness))) + parseFloat(d.acousticness))).toString(16)});
 
 
     
      // plot the bottom and side axis
       SCATTER_FRAME.append("g")
-     .attr("transform", "translate(" + (MARGINS.left) + "," + 
-     (SCATTER_HEIGHT/2 + MARGINS.top) + ")")
-     .call(d3.axisBottom(X_SCALE2).ticks(10))
+     .attr("transform", "translate(" + (2 * MARGINS.left) + "," + 
+     (SCATTER_HEIGHT + MARGINS.top) + ")")
+     .call(d3.axisBottom(X_SCALE).ticks(10))
          .attr("font-size", "12px");
 
     
 
     SCATTER_FRAME.append("g")
-        .attr("transform", "translate("  + (SCATTER_WIDTH/2 + MARGINS.left) + "," + 
-        (MARGINS.top)+  ")")
-        .call(d3.axisLeft(Y_SCALE2).ticks(10))
+        .attr("transform", "translate("  + (2 * MARGINS.left) + "," + 
+        (MARGINS.top) +  ")")
+        .call(d3.axisLeft(Y_SCALE).ticks(10))
             .attr("font-size", "12px");
     
 
